4d0d08c5957e8d66375ced0d0e478f86c636a900
Ghany AE <ghany.aersa@fifgroup.astra.co.id>
2021-06-16 14:13:33 +0700

    add handle cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
 
 const response = require("./src/helpers/response");
