851a535858e276325d3fee098755319fc8213c86
Ghany AE <ghany.aersa@fifgroup.astra.co.id>
2021-06-19 01:41:21 +0700

    add aditional

--- src/controllers/public/dummy/userDummy.js
+++ src/controllers/public/dummy/userDummy.js
@@ -6,18 +6,21 @@ const service = async function (req, res, next) {
       name: "Ghany Abdillah Ersa",
       email: "ghanyersa24@gmail.com",
       role: "admin",
+      password: "$2b$10$vrK8zMSGR3K5ZspjEx4EAe.85Pi10.KW65b2IjrqyiO.xwNZUgCQK",
     },
     {
       id: "0734f6b2-6b4c-45b9-be8c-d1aaf6803349",
       name: "Danang TP",
       email: "danang.tp@gmail.com",
       role: "admin",
+      password: "$2b$10$vrK8zMSGR3K5ZspjEx4EAe.85Pi10.KW65b2IjrqyiO.xwNZUgCQK",
     },
     {
       id: "0734f6b2-6b4c-45b9-be8c-d1aaf6803350",
       name: "Ridwan Fajar",
       email: "rdwn@gmail.com",
       role: "writer",
+      password: "$2b$10$vrK8zMSGR3K5ZspjEx4EAe.85Pi10.KW65b2IjrqyiO.xwNZUgCQK",
     },
   ]);
   next();
