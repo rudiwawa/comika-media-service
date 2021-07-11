8020a35eaec6f52992b73f003750a2a2020ab32f
Ghany AE <ghany.aersa@fifgroup.astra.co.id>
2021-07-11 09:40:07 +0700

    edit path jumbotron

--- src/controllers/jumbotrons/create.jumbotronController.js
+++ src/controllers/jumbotrons/create.jumbotronController.js
@@ -1,17 +1,19 @@
 const { Jumbotron } = require("../../models");
 const service = async function (req, res, next) {
   const payload = {
-    img: req.file.path,
     sequence: req.body.sequence,
     link: req.body.link,
     isPhone: req.body.isPhone,
     isDekstop: req.body.isDekstop,
   };
+  if (req.file) {
+    payload.img = req.urlApps + req.file.path;
+  }
   try {
     const requestDB = await Jumbotron.create(payload);
     res.response = { msg: "jumbotron berhasil ditambahkan", data: requestDB };
   } catch (error) {
-    res.response = { status: 500, msg: error.message };
+    res.response = { status: 500, msg: error.message, data: error };
   }
   next();
 };
