14e9f749fac898170e9f288d6300f13f71e4453e
Ghany AE <ghany.aersa@fifgroup.astra.co.id>
2021-08-29 23:07:50 +0700

    perbaikan record prod

--- src/middlewares/record.js
+++ src/middlewares/record.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  req.urlApps = req.protocol + "://" + "service-comika.herokuapp.com" + "/";
+  req.urlApps = req.protocol + "://" + "api.comika.media/" + "/";
   req.record = {
     url: req.url + req.originalUrl,
     method: req.method,
