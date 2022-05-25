6e11f629fac7bc1e64b1cf3183214fbb2919456a
ghany abdillah ersa <ghanyersa24@gmail.com>
2022-05-26 02:03:41 +0700

    add comment reply and notification

--- src/controllers/comments/create.commentController.js
+++ src/controllers/comments/create.commentController.js
@@ -1,28 +1,130 @@
 const {
   Comment,
+  Article,
+  User,
   Sequelize: { Op },
-} = require("../../models");
-const { body } = require("express-validator");
-const moment = require("moment");
+} = require('../../models');
+const { body } = require('express-validator');
+const sendNotification = require('../../services/sendNotification');
 const service = async function (req, res, next) {
   try {
     const payload = {
       articleId: req.article.id,
       userId: req.auth.id,
       comment: req.body.comment,
+      commentId: null,
     };
+    if (req.body.commentId) {
+      payload.commentId = await parentComment(
+        req.body.commentId,
+        req.article.id
+      );
+    }
     const requestDB = await Comment.create(payload);
     res.response = { data: requestDB };
+    await broadcastNotification(
+      req.auth.name,
+      payload.comment,
+      req.article.title,
+      payload.commentId,
+      payload.userId
+    );
   } catch (error) {
     res.response = { status: 500, msg: error.message };
   }
   next();
 };
+
+const parentComment = async (commentId, articleId) => {
+  const comment = await Comment.findOne({
+    where: {
+      id: commentId,
+      articleId,
+    },
+  });
+  if (!comment) {
+    return null;
+  }
+  if (comment.commentId) {
+    return parentComment(comment.commentId, articleId);
+  }
+  return comment.id;
+};
+
 const validation = [
-  body("comment")
+  body('comment')
     .notEmpty()
-    .withMessage("komentar tidak boleh kosong")
+    .withMessage('komentar tidak boleh kosong')
     .isLength({ min: 10 })
-    .withMessage("komentar minimal 10 karakter"),
+    .withMessage('komentar minimal 10 karakter'),
 ];
+const broadcastNotification = async (
+  name,
+  comment,
+  articleName,
+  commentId,
+  userParentCommentId
+) => {
+  // paralel get list user id admin and writer
+  const requestUser = await Promise.all([
+    getListUserIdAdminAndWriter(userParentCommentId),
+    getListUserInComment(commentId, userParentCommentId),
+  ]);
+  let listUserNotAdminAndWriter = [];
+  const listAdmin = requestUser[0];
+  // get list user id who not in list user id admin and writer
+  if (commentId) {
+    const listUser = requestUser[1];
+    if (listUser)
+      listUserNotAdminAndWriter = listUser.filter(
+        (user) => !listAdmin.includes(user.id)
+      );
+  }
+  const listUserReceiveNotification = [
+    ...listUserNotAdminAndWriter,
+    ...listAdmin,
+  ];
+  // send notification to list user id
+  sendNotification.bulkCreate(
+    listUserReceiveNotification,
+    `${name} melakukan komentar di artikel ${articleName}`,
+    `${name} said "${comment}"`,
+    'https://api.comika.media/uploads/comika/icon.png',
+    null,
+    'informasi'
+  );
+};
+const getListUserInComment = async (commentId, userParentCommentId) => {
+  if (!commentId) return [];
+  const comments = await Comment.findAll({
+    attributes: ['userId'],
+    where: {
+      commentId,
+      userId: {
+        [Op.ne]: userParentCommentId,
+      },
+    },
+  });
+  return comments.map((item) => item.userId);
+};
+
+const getListUserIdAdminAndWriter = async (userParentCommentId) => {
+  const admin = await User.findAll({
+    attributes: ['id'],
+    where: {
+      [Op.or]: [
+        {
+          role: 'admin',
+        },
+        {
+          role: 'writer',
+        },
+      ],
+      id: {
+        [Op.ne]: userParentCommentId,
+      },
+    },
+  });
+  return admin.map((item) => item.id);
+};
 module.exports = { service, validation };
--- src/services/sendNotification.js
+++ src/services/sendNotification.js
@@ -1,18 +1,18 @@
-const { Notification } = require("../models");
+const { Notification } = require('../models');
 
 const create = async function (userId, title, description, img, orderId, type) {
   try {
     const payload = {
       title,
       description: description
-        .replace(/(<([^>]+)>)/gi, "")
-        .replace(/  /gi, "")
-        .replace(/\n/gi, "")
+        .replace(/(<([^>]+)>)/gi, '')
+        .replace(/  /gi, '')
+        .replace(/\n/gi, '')
         .substring(0, 120),
-      descriptionHtml: description.replace(/  /gi, "").replace(/\n/gi, ""),
+      descriptionHtml: description.replace(/  /gi, '').replace(/\n/gi, ''),
       userId,
-      type: type || "transaksi",
-      img: img || "https://api.comika.media/uploads/comika/checkout.png",
+      type: type || 'transaksi',
+      img: img || 'https://api.comika.media/uploads/comika/checkout.png',
       orderId,
     };
     return await Notification.create(payload);
@@ -20,5 +20,35 @@ const create = async function (userId, title, description, img, orderId, type) {
     throw new Error(error.toString());
   }
 };
+const bulkCreate = async function (
+  listUserId,
+  title,
+  description,
+  img,
+  orderId,
+  type
+) {
+  try {
+    const payload = {
+      title,
+      description: description
+        .replace(/(<([^>]+)>)/gi, '')
+        .replace(/  /gi, '')
+        .replace(/\n/gi, '')
+        .substring(0, 120),
+      descriptionHtml: description.replace(/  /gi, '').replace(/\n/gi, ''),
+      type: type || 'transaksi',
+      img: img || 'https://api.comika.media/uploads/comika/checkout.png',
+      orderId,
+    };
+    const payloads = listUserId.map((userId) => ({
+      ...payload,
+      userId,
+    }));
+    return await Notification.bulkCreate(payloads);
+  } catch (error) {
+    throw new Error(error.toString());
+  }
+};
 
-module.exports = { create };
+module.exports = { create, bulkCreate };
